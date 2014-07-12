17493f04e87ccd2d824328fd1f391656ed4bac14
jdeaza <jdeaza@gmail.com>
2014-07-11 21:02:32 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,22 @@
+#########Course project 1 Exploratory Data Analysis
+
+######Read data
+
+data_power <- read.table("./exploratory/household_power_consumption.txt", sep = ";", dec = ".", header = TRUE, na.string = "?")
+
+#####Subsetting data
+
+dataf <- subset(data_power, Date == "1/2/2007" | Date == "2/2/2007")
+
+#######Create and paste a new variable for Date an Time variables
+
+dataf$datetime <- strptime(paste(dataf$Date, dataf$Time), , format = "%d/ %m/ %Y %H: %M: %S")
+
+########Graph 2 ########
+
+with(dataf, plot(datetime, Global_active_power, type = "l", xlab = " ", ylab = "Global Active Power (kilowatts)"))
+
+########Saving plott in PNG file
+
+dev.copy(png,file = "plot2.png", width = 480, height = 480)
+dev.off()
